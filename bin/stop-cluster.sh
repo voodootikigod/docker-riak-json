5f0dcadc7eccc1c08515482a377c879b051c399f
Chris Williams <chris@iterativedesigns.com>
2014-04-02 16:21:03 -0500

    Removing things that are not used.

--- bin/stop-cluster.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -e
-
-if sudo docker ps | grep "voodootikigod/riak-json" >/dev/null; then
-  sudo docker ps | grep "voodootikigod/riak-json" | awk '{ print $1 }' | xargs -r sudo docker kill >/dev/null
-  echo "Stopped the cluster and cleared all of the running containers."
-fi
--- bin/test-cluster.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -e
-
-for index in `seq 5`;
-do
-  echo "Testing [riak${index}]...`curl -s http://33.33.33.${index}0:8098/ping`"
-done
